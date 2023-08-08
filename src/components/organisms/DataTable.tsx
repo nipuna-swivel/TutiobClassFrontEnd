7e2dae8d7a38dafc783e033341e7611cf16de313
nipuna-swivel <nipuna@swivelgroup.com.au>
2023-08-08 14:24:48 +0530

    remove buildin codes

--- src/components/organisms/DataTable.tsx
+++ src/components/organisms/DataTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useEffect, useCallback } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -9,28 +9,10 @@ import Paper from "@mui/material/Paper";
 import { useAppDispatch, useAppSelector } from "@/hooks";
 import { fetchStudents } from "@/features/student/studentSlice";
 import DeleteIcon from "@mui/icons-material/Delete";
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
 
-function createData(
-	name: string,
-	calories: number,
-	fat: number,
-	carbs: number,
-	protein: number
-) {
-	return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-	createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-	createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-	createData("Eclair", 262, 16.0, 24, 6.0),
-	createData("Cupcake", 305, 3.7, 67, 4.3),
-	createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
-export default function BasicTable() {
+export default function StudentTable() {
 	const dispatch = useAppDispatch();
 	const { students } = useAppSelector((state) => state.student);
 
@@ -71,7 +53,7 @@ export default function BasicTable() {
 							<TableCell align="right">{student.classLocation}</TableCell>
 							<TableCell align="right">
 								{" "}
-                <IconButton aria-label="edit"  color="primary">
+								<IconButton aria-label="edit" color="primary">
 									<EditIcon />
 								</IconButton>
 								<IconButton aria-label="delete" color="secondary">
