5603344b19f846a10df6db7e5228b58874dbb652
nipuna-swivel <nipuna@swivelgroup.com.au>
2023-08-15 16:59:37 +0530

    ignoreBuildErrors: true,

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    typescript: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        
+    ignoreBuildErrors: true,
+      },
+}
 
 module.exports = nextConfig
