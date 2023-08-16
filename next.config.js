c50cb4753d0245a60971bb3aee7f23384121e2c9
nipuna-swivel <nipuna@swivelgroup.com.au>
2023-08-16 10:30:39 +0530

    disable lint

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     typescript: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        
-    ignoreBuildErrors: true,
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
       },
 }
 
