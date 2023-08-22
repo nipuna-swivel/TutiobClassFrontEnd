30ea661eba1a93d04c5630cd7865723c2516121c
nipuna-swivel <nipuna@swivelgroup.com.au>
2023-08-22 14:40:41 +0530

    Intercepters build issue fixed

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,30 @@
 import axios from "axios";
-const accessToken = localStorage.getItem("accessToken");
 
+// Create an Axios instance
 const axiosInstance = axios.create({
 	baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
 	headers: {
 		"Content-Type": "application/json",
-		Authorization: "Bearer " + accessToken,
 	},
 });
 
+// Add a request interceptor
+axiosInstance.interceptors.request.use(
+	(config) => {
+		// Get the token from localStorage
+		const accessToken = localStorage.getItem("accessToken");
+
+		if (accessToken) {
+			// Add the token to the Authorization header
+			config.headers["Authorization"] = `Bearer ${accessToken}`;
+		}
+
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+// Export the Axios instance with interceptors
 export { axiosInstance as axios };
